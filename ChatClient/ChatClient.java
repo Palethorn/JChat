4eb13652ae68240f46f951e3577c4080d9b2a0b7
David <wizzard405@gmail.com>
2012-01-18 21:30:16 +0100

    Args error in client added

--- ChatClient/ChatClient.java
+++ ChatClient/ChatClient.java
@@ -1,20 +1,33 @@
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-public class ChatClient {
-	static Client client;
-	public static void main(String[] args) {
-		try {
-			if(args.length > 1)
-			{
-				client = new Client(args[0], Integer.parseInt(args[1]));
-			}
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Couldn't connect.\n");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
+public class ChatClient
+{
+    static Client client;
+
+    public static void main(String [] args)
+    {
+        try
+        {
+            if(args.length > 1)
+            {
+                client = new Client(args[0], Integer.parseInt(args[1]));
+            }
+            else
+            {
+                System.out
+                        .println(" Remote server address and port must be specified");
+            }
+        }
+        catch(UnknownHostException e)
+        {
+            e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Couldn't connect.\n");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
